739bd25810ab2eb9e3417ee8b7ed568a5c23899a
Keijiro Takahashi <keijiro@gmail.com>
2021-06-29 17:28:04 +0900

    Sender implementation

--- Packages/jp.keijiro.klak.spout/Runtime/SpoutSender.cs
+++ Packages/jp.keijiro.klak.spout/Runtime/SpoutSender.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using IntPtr = System.IntPtr;
 
 namespace Klak.Spout {
 
@@ -19,16 +18,58 @@ public sealed partial class SpoutSender : MonoBehaviour
 
     #endregion
 
+    #region Buffer texture object
+
+    RenderTexture _buffer;
+
+    void PrepareBuffer(int width, int height)
+    {
+        if (_buffer != null &&
+            (_buffer.width != width || _buffer.height != height))
+        {
+            Utility.Destroy(_buffer);
+            _buffer = null;
+        }
+
+        if (_buffer == null)
+        {
+            _buffer = new RenderTexture(width, height, 0);
+            _buffer.hideFlags = HideFlags.DontSave;
+        }
+    }
+
+    #endregion
+
     #region MonoBehaviour implementation
 
     void OnDisable()
       => ReleaseSender();
 
+    void OnDestroy()
+    {
+        Utility.Destroy(_buffer);
+        _buffer = null;
+    }
+
     void Update()
     {
+        if (_captureMethod == CaptureMethod.Texture)
+        {
+            PrepareBuffer(_sourceTexture.width, _sourceTexture.height);
+            Blitter.Blit(_sourceTexture, _buffer);
+        }
+
+        if (_captureMethod == CaptureMethod.GameView)
+        {
+            PrepareBuffer(Screen.width, Screen.height);
+            var temp = RenderTexture.GetTemporary(Screen.width, Screen.height, 0);
+            ScreenCapture.CaptureScreenshotIntoRenderTexture(temp);
+            Graphics.Blit(temp, _buffer);
+            RenderTexture.ReleaseTemporary(temp);
+        }
+
         // Sender lazy initialization
-        if (_sender == null)
-            _sender = new Sender(_sourceName, _sourceTexture);
+        if (_sender == null) _sender = new Sender(_sourceName, _buffer);
 
         // Sender plugin-side update
         _sender.Update();
